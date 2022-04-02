a275deec7e42b214818c1b135b8031c313885007
Vlad Shchehlov <vladyslav.shchehlov@sigma.software>
2022-04-02 23:18:18 +0300

    initial commit

---
+++ src/main/java/com/veglad/servicesmarthome/ServiceSmartHomeApplication.java
@@ -0,0 +1,13 @@
+package com.veglad.servicesmarthome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSmartHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSmartHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veglad/servicesmarthome/ServiceSmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veglad.servicesmarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
