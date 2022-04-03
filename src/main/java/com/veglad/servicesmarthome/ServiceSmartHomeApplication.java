ff00d93dba0644a456f432603f58bf6ffae88533
Vlad Shchehlov <vladyslav.shchehlov@sigma.software>
2022-04-03 14:14:22 +0300

    Default skeleton

--- src/main/java/com/veglad/servicesmarthome/ServiceSmartHomeApplication.java
+++ src/main/java/com/veglad/servicesmarthome/ServiceSmartHomeApplication.java
@@ -9,5 +9,4 @@ public class ServiceSmartHomeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceSmartHomeApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/veglad/servicesmarthome/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.veglad.servicesmarthome.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> builder.modulesToInstall(new JavaTimeModule());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter appJackson2HttpMessageConverter(Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
+        return new MappingJackson2HttpMessageConverter(jackson2ObjectMapperBuilder.createXmlMapper(false).build());
+    }
+
+    @Bean(value = {"json", "objectMapper"})
+    public ObjectMapper objectMapper(MappingJackson2HttpMessageConverter appJackson2HttpMessageConverter) {
+        return appJackson2HttpMessageConverter.getObjectMapper();
+    }
+}
